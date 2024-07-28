b9aca87f8de28bad9f3788a8c1c3643a2093384e
tuc56 <57573828+tuc56@users.noreply.github.com>
2024-07-27 20:18:55 -0500

    Add enderio files I've already done, commented out

---
+++ src/main/java/com/blockgoblin31/modtweaker/enderio/natives/SoulDataRegistry.java
@@ -0,0 +1,28 @@
+package com.blockgoblin31.modtweaker.enderio.natives;
+/*
+import com.blamejared.crafttweaker.api.annotation.ZenRegister;
+import com.blamejared.crafttweaker.api.data.IData;
+import com.blamejared.crafttweaker.api.data.MapData;
+import com.blamejared.crafttweaker_annotations.annotations.Document;
+import com.enderio.machines.common.souldata.SoulData;
+import com.mojang.datafixers.kinds.App;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import org.openzen.zencode.java.ZenCodeType;
+
+@ZenRegister
+@Document("mods/enderio/natives/SoulData")
+@ZenCodeType.Name("mods.enderio.SoulData")
+public class SoulDataRegistry{
+
+    @ZenCodeType.Method
+    public static void registerCodec(String name, MapData types) {
+
+    }
+}
+*/
